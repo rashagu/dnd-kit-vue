53c42a6470c4124da800d96696852c7421208ffe
smw <1135192606@qq.com>
2022-12-29 15:50:05 +0800

    chore: package.json

--- packages/sortable/test/SortableDemo1.spec.tsx
+++ packages/sortable/test/SortableDemo1.spec.tsx
@@ -5,5 +5,5 @@ import {mount} from "@vue/test-utils";
 test('SortableDemo1 test', async () => {
   const wrapper = mount(Comp, {})
   const text = wrapper.findAll('div[aria-roledescription="sortable"]')
-  expect(text.length).toEqual(3)
+  expect(text.length).toEqual(10)
 })
